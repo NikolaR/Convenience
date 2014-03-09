21458a8e5b122d6a227ec1b0999b02735c5766af
Nikola Radosavljevic <nikola.radosavljevic@outlook.com>
2014-03-09 02:40:38 +0100

    Bumped version number

--- Convenience/Properties/AssemblyInfo.cs
+++ Convenience/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Convenience")]
-[assembly: AssemblyDescription("Convenience helper classes")]
+[assembly: AssemblyDescription("Convenient .NET types and methods missing from BCL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nikola Radosavljevic")]
 [assembly: AssemblyProduct("ConvenienceLib")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.9")]
-[assembly: AssemblyFileVersion("0.1.0.9")]
+[assembly: AssemblyVersion("0.2.0.1")]
+[assembly: AssemblyFileVersion("0.2.0.1")]
